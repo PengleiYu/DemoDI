b00015b09501001a2348829205f67b27d373c067
yupenglei <yupengniaosanshi@gmail.com>
2022-01-16 11:37:19 +0800

    使用@Providers

--- hiltdemo/src/main/java/com/utopia/hiltdemo/datasource.kt
+++ hiltdemo/src/main/java/com/utopia/hiltdemo/datasource.kt
@@ -1,9 +1,10 @@
 package com.utopia.hiltdemo
 
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import retrofit2.Retrofit
 import javax.inject.Inject
 
 interface AnalyticsService {
@@ -17,26 +18,27 @@ class AnalyticsServiceImpl @Inject constructor() : AnalyticsService {
 
 class AnalyticsAdapter @Inject constructor(val service: AnalyticsService)
 
-@Module
-@InstallIn(ActivityComponent::class)
-abstract class AnalyticsModule {
-  // binds是为了绑定接口和实现类
-  @Binds
-  abstract fun bindAnalyticsService(
-    analyticsServiceImpl: AnalyticsServiceImpl
-  ): AnalyticsService
-}
-
 //@Module
 //@InstallIn(ActivityComponent::class)
-//object AnalyticsModule2 {
-//  @Provides
-//  fun provideAnalyticsService(
-//    //
-//  ): AnalyticsService {
-//    return Retrofit.Builder()
-//      .baseUrl("https://example.com")
-//      .build()
-//      .create(AnalyticsService::class.java)
-//  }
+//abstract class AnalyticsModule {
+//  // binds是为了绑定接口和实现类
+//  @Binds
+//  abstract fun bindAnalyticsService(
+//    analyticsServiceImpl: AnalyticsServiceImpl
+//  ): AnalyticsService
 //}
+
+@Module
+@InstallIn(ActivityComponent::class)
+object AnalyticsModule2 {
+  // provides用于为接口提供第三方实现类
+  @Provides
+  fun provideAnalyticsService(
+    //
+  ): AnalyticsService {
+    return Retrofit.Builder()
+      .baseUrl("https://example.com")
+      .build()
+      .create(AnalyticsService::class.java)
+  }
+}
